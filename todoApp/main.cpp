576c645419fd6f928f852342b29642511b82d14d
balintkemeny <balintkemeny@gmail.com>
2019-10-03 11:31:29 +0200

    Solved print usage

--- todoApp/main.cpp
+++ todoApp/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
+void printUsage() {
+    std::cout << "Command Line Todo application" << std::endl;
+    std::cout << "=============================" << std::endl;
+    std::cout << "\n" << "Command line arguments:" << std::endl;
+    std::cout << "      -l      Lists all the tasks" << std::endl;
+    std::cout << "      -a      Adds a new task" << std::endl;
+    std::cout << "      -r      Removes a task" << std::endl;
+    std::cout << "      -c      Completes a task" << std::endl;
+}
+
 int main(int argc, char** argv) {
     if (argc == 1) {
-        std::cout << "Command Line Todo application" << std::endl;
-        std::cout << "=============================" << std::endl;
-        std::cout << "\n" << "Command line arguments:" << std::endl;
-        std::cout << "      -l      Lists all the tasks" << std::endl;
-        std::cout << "      -a      Adds a new task" << std::endl;
-        std::cout << "      -r      Removes a task" << std::endl;
-        std::cout << "      -c      Completes a task" << std::endl;
+        printUsage();
     };
     return 0;
 }
